6a27c6bc767fe121f5b9c98f1f8bc6d9826fae35
m3mpm <m3mpm@mymail.ru>
2023-12-14 15:22:17 +0700

    init project

---
+++ src/test/java/org/m3mpm/webinfo/JavaWebInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.m3mpm.webinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
