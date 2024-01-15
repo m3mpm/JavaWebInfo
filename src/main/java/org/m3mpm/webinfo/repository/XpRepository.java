68ed3592f0c361104f2119b5099b317a26ddf7aa
m3mpm <m3mpm@mymail.ru>
2024-01-15 19:14:17 +0700

    fix mistake in XpRepository interface

--- src/main/java/org/m3mpm/webinfo/repository/XpRepository.java
+++ src/main/java/org/m3mpm/webinfo/repository/XpRepository.java
@@ -1,6 +1,7 @@
 package org.m3mpm.webinfo.repository;
 
+import org.m3mpm.webinfo.model.Xp;
 import org.springframework.data.repository.CrudRepository;
 
-public interface XpRepository extends CrudRepository {
+public interface XpRepository extends CrudRepository<Xp,Long> {
 }
