d9002e053bf7aac757da08a1c50d3a863b49b37d
m3mpm <m3mpm@mymail.ru>
2024-01-29 19:28:00 +0700

    add TaskMapper setting for convert to TaskDto & Task, fail
    1. Add configuring TaskMapper and  add Coverter<S,T> to convert from Task to
       TasDto and reverse.
       Result: FAILD. Need fix the problem.

--- src/main/java/org/m3mpm/webinfo/config/ProjectConfig.java
+++ src/main/java/org/m3mpm/webinfo/config/ProjectConfig.java
@@ -1,14 +1,23 @@
 package org.m3mpm.webinfo.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(PRIVATE);
+        return mapper;
     }
 }
--- src/main/java/org/m3mpm/webinfo/mapper/TaskMapper.java
+++ src/main/java/org/m3mpm/webinfo/mapper/TaskMapper.java
@@ -2,9 +2,12 @@ package org.m3mpm.webinfo.mapper;
 
 import org.m3mpm.webinfo.dto.TaskDto;
 import org.m3mpm.webinfo.model.Task;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class TaskMapper {
@@ -15,6 +18,55 @@ public class TaskMapper {
     public TaskMapper(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
     }
+    
+    public void setupMapper(){
+        modelMapper.createTypeMap(Task.class,TaskDto.class).addMappings(m->m.skip(TaskDto::setParentTask)).setPostConverter(toTaskDtoConverter());
+        modelMapper.createTypeMap(TaskDto.class,Task.class).addMappings(m->m.skip(Task::setParentTask)).setPostConverter(toTaskConverter());
+    }
+
+    protected Converter<Task,TaskDto> toTaskDtoConverter(){
+        return context -> {
+            Task source = context.getSource();
+            TaskDto destination = context.getDestination();
+            mapSpecificFields(source, destination);
+            return context.getDestination();
+        };
+    }
+
+    void mapSpecificFields(Task source, TaskDto destination){
+//        Task parentTask = source.getParentTask();
+//        if(parentTask != null ) destination.setParentTask(parentTask.getTitle());
+        destination.setParentTask(Objects.isNull(source) ? null : source.getTitle());
+    }
+
+    protected Converter<TaskDto, Task> toTaskConverter() {
+        return context -> {
+            TaskDto source = context.getSource();
+            Task destination = context.getDestination();
+            mapSpecificFields(source, destination);
+            return context.getDestination();
+        };
+    }
+
+    void mapSpecificFields(TaskDto source, Task destination)  {
+        String parentName = source.getParentTask();
+        if (parentName == null || parentName.isEmpty()) {
+            destination.setParentTask(null);
+        } else {
+            Task parentTask = new Task();
+            parentTask.setTitle(parentName);
+            destination.setParentTask(parentTask);
+        }
+    }
+
+//    public void mapSpecificFields(Droid source, DroidDto destination) {
+//        destination.setUnicornId(Objects.isNull(source) || Objects.isNull(source.getId()) ? null : source.getUnicorn().getId());
+//    }
+
+//    void mapSpecificFields(DroidDto source, Droid destination) {
+//        destination.setUnicorn(unicornRepository.findById(source.getUnicornId()).orElse(null));
+//    }
+
 
     public TaskDto converToTaskDto(Task task) {
         return modelMapper.map(task,TaskDto.class);
