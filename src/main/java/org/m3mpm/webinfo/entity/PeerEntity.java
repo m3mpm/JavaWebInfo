23211f6d6b2e95d1c7241000cfd6080c8c326a14
Ruslan <m3mpm@users.noreply.github.com>
2025-05-26 17:23:34 +0700

    added Peer Entity

---
+++ src/main/java/org/m3mpm/webinfo/entity/PeerEntity.java
@@ -0,0 +1,96 @@
+package org.m3mpm.webinfo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+
+@Entity
+@Table(name="peers")
+public class PeerEntity {
+
+    @Id
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name="birthday")
+    private LocalDate birthday;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public PeerEntity() {
+    }
+
+    public PeerEntity(String nickname, LocalDate birthday) {
+        this.nickname = nickname;
+        this.birthday = birthday;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "PeerEntity{" +
+                "nickname='" + nickname + '\'' +
+                ", birthday=" + birthday +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PeerEntity that = (PeerEntity) o;
+        return Objects.equals(nickname, that.nickname) && Objects.equals(birthday, that.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nickname, birthday);
+    }
+}
