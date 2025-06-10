5e0d8b96ffb26953af6f5b3ddd0e33ccae7d1456
Ruslan <m3mpm@users.noreply.github.com>
2025-06-10 19:31:03 +0700

    Checks, added CheckEntity class
    1. Use ManyToOne to Peer & Task

---
+++ src/main/java/org/m3mpm/webinfo/entity/CheckEntity.java
@@ -0,0 +1,45 @@
+package org.m3mpm.webinfo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "checks")
+public class CheckEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "peer", referencedColumnName = "nickname")
+    private PeerEntity peer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task", referencedColumnName = "title")
+    private TaskEntity task;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
