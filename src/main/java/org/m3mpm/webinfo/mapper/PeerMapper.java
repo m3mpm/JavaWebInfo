c45ebc25a345d525609f5027207df248e1d04cf5
Ruslan <m3mpm@users.noreply.github.com>
2025-06-11 19:00:54 +0700

    Peers, changed PeerMapper
    1. PeerMapper has become a abstract class
    2. Added peerEntityToString() & stringToPeerEntity() for CheckMapper

--- src/main/java/org/m3mpm/webinfo/mapper/PeerMapper.java
+++ src/main/java/org/m3mpm/webinfo/mapper/PeerMapper.java
@@ -2,19 +2,39 @@ package org.m3mpm.webinfo.mapper;
 
 import org.m3mpm.webinfo.dto.PeerDto;
 import org.m3mpm.webinfo.entity.PeerEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValueCheckStrategy;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.m3mpm.webinfo.service.PeerService;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 @Mapper(componentModel = "spring",
         nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-public interface PeerMapper {
+public abstract class PeerMapper {
 
-    PeerDto peerToPeerDto(PeerEntity peer);
 
-    PeerEntity peerDtoToPeer(PeerDto peerDto);
+    private final PeerService peerService;
 
-    void updatePeerFromDto(PeerDto peerDto, @MappingTarget PeerEntity peerEntity);
+    @Autowired
+    public PeerMapper(PeerService peerService) {
+        this.peerService = peerService;
+    }
+
+    public abstract PeerDto peerToPeerDto(PeerEntity peer);
+
+    public abstract PeerEntity peerDtoToPeer(PeerDto peerDto);
+
+    public abstract void updatePeerFromDto(PeerDto peerDto, @MappingTarget PeerEntity peerEntity);
+
+    @Named("peerEntityToString")
+    String peerEntityToString(PeerEntity peerEntity) {
+        return peerEntity.getNickname();
+    }
+
+    @Named("stringToPeerEntity")
+    PeerEntity stringToPeerEntity(String nickname) {
+        return peerService.getPeerById(nickname).map(this::peerDtoToPeer).orElse(new PeerEntity());
+    }
+
 }
