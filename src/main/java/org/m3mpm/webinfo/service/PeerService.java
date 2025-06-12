f2d23ca4d56b514d8a534cb9b35c8cb864624476
Ruslan <m3mpm@users.noreply.github.com>
2025-06-12 15:55:04 +0700

    Peers, created a PeerService interface instead of a class
    1. Created PeerService interface
    2. Renamed PeerService class to PeerServiceImp

--- src/main/java/org/m3mpm/webinfo/service/PeerService.java
+++ src/main/java/org/m3mpm/webinfo/service/PeerService.java
@@ -1,88 +1,15 @@
 package org.m3mpm.webinfo.service;
 
-import jakarta.transaction.Transactional;
 import org.m3mpm.webinfo.dto.PeerDto;
-import org.m3mpm.webinfo.entity.PeerEntity;
-import org.m3mpm.webinfo.mapper.PeerMapper;
-import org.m3mpm.webinfo.repository.PeerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-@Service
-public class PeerService {
-
-    private final PeerRepository peerRepository;
-
-    private final PeerMapper peerMapper;
-
-    @Autowired
-    public PeerService(PeerRepository peerRepository, PeerMapper peerMapper) {
-        this.peerRepository = peerRepository;
-        this.peerMapper = peerMapper;
-    }
-
-    /* ver.1 */
-//    public Optional<List<PeerDto>> getAllPeers() {
-//        List<PeerEntity> peersList = peerRepository.findAll();
-//        return peersList.isEmpty() ? Optional.empty() : Optional.of(peersList.stream().map(peerMapper::peerToPeerDto).collect(Collectors.toList()));
-//    }
-
-    /* ver.2 */
-//    public Optional<List<PeerDto>> getAllPeers() {
-//        List<PeerEntity> peersList = peerRepository.findAll();
-//
-//            return Optional.ofNullable(peersList.isEmpty() ? null : peersList.stream()
-//                .map(peerMapper::peerToPeerDto)
-//                .collect(Collectors.toList()));
-//    }
-
-    /* ver.3 */
-    public List<PeerDto> getAllPeers() {
-        List<PeerEntity> peersList = peerRepository.findAll();
-        return peersList.stream()
-                .map(peerMapper::peerToPeerDto)
-                .collect(Collectors.toList());
-    }
-
-    public Optional<PeerDto> getPeerById(String id) {
-        Optional<PeerEntity> optionalPeerEntity = peerRepository.findById(id);
-        return optionalPeerEntity.map(peerMapper::peerToPeerDto);
-    }
-
-    public Optional<PeerDto> getPeerByNickname(String nickname) {
-        Optional<PeerEntity> optionalPeerEntity = peerRepository.findPeerEntityByNickname(nickname);
-        return optionalPeerEntity.map(peerMapper::peerToPeerDto);
-    }
-
-    public boolean savePeer(PeerDto peerDto) {
-        boolean added = false;
-        if(!peerRepository.existsById(peerDto.getNickname())){
-            peerRepository.save(peerMapper.peerDtoToPeer(peerDto));
-            added = true;
-        }
-        return added;
-    }
-
-    public boolean deletePeer(PeerDto deletePeerDto) {
-        boolean deleted = false;
-        if(peerRepository.existsById(deletePeerDto.getNickname())){
-            peerRepository.deleteById(deletePeerDto.getNickname());
-            deleted = true;
-        }
-        return deleted;
-    }
-
-    @Transactional
-    public void updatePeer(PeerDto editPeerDto) {
-        Optional<PeerEntity> optionalPeerEntity = peerRepository.findById(editPeerDto.getNickname());
-        if(optionalPeerEntity.isPresent()){
-            PeerEntity peerEntity = optionalPeerEntity.get();
-            peerMapper.updatePeerFromDto(editPeerDto, peerEntity);
-            peerRepository.save(peerEntity);
-        }
-    }
+public interface PeerService {
+    List<PeerDto> getAllPeers();
+    Optional<PeerDto> getPeerById(String id);
+    Optional<PeerDto> getPeerByNickname(String name);
+    boolean savePeer(PeerDto peerDto);
+    boolean deletePeer(PeerDto peerDto);
+    void updatePeer(PeerDto peerDto);
 }
---
+++ src/main/java/org/m3mpm/webinfo/service/PeerServiceImp.java
@@ -0,0 +1,88 @@
+package org.m3mpm.webinfo.service;
+
+import jakarta.transaction.Transactional;
+import org.m3mpm.webinfo.dto.PeerDto;
+import org.m3mpm.webinfo.entity.PeerEntity;
+import org.m3mpm.webinfo.mapper.PeerMapper;
+import org.m3mpm.webinfo.repository.PeerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class PeerServiceImp implements PeerService {
+
+    private final PeerRepository peerRepository;
+
+    private final PeerMapper peerMapper;
+
+    @Autowired
+    public PeerServiceImp(PeerRepository peerRepository, PeerMapper peerMapper) {
+        this.peerRepository = peerRepository;
+        this.peerMapper = peerMapper;
+    }
+
+    /* ver.1 */
+//    public Optional<List<PeerDto>> getAllPeers() {
+//        List<PeerEntity> peersList = peerRepository.findAll();
+//        return peersList.isEmpty() ? Optional.empty() : Optional.of(peersList.stream().map(peerMapper::peerToPeerDto).collect(Collectors.toList()));
+//    }
+
+    /* ver.2 */
+//    public Optional<List<PeerDto>> getAllPeers() {
+//        List<PeerEntity> peersList = peerRepository.findAll();
+//
+//            return Optional.ofNullable(peersList.isEmpty() ? null : peersList.stream()
+//                .map(peerMapper::peerToPeerDto)
+//                .collect(Collectors.toList()));
+//    }
+
+    /* ver.3 */
+    public List<PeerDto> getAllPeers() {
+        List<PeerEntity> peersList = peerRepository.findAll();
+        return peersList.stream()
+                .map(peerMapper::peerToPeerDto)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<PeerDto> getPeerById(String id) {
+        Optional<PeerEntity> optionalPeerEntity = peerRepository.findById(id);
+        return optionalPeerEntity.map(peerMapper::peerToPeerDto);
+    }
+
+    public Optional<PeerDto> getPeerByNickname(String nickname) {
+        Optional<PeerEntity> optionalPeerEntity = peerRepository.findPeerEntityByNickname(nickname);
+        return optionalPeerEntity.map(peerMapper::peerToPeerDto);
+    }
+
+    public boolean savePeer(PeerDto peerDto) {
+        boolean added = false;
+        if(!peerRepository.existsById(peerDto.getNickname())){
+            peerRepository.save(peerMapper.peerDtoToPeer(peerDto));
+            added = true;
+        }
+        return added;
+    }
+
+    public boolean deletePeer(PeerDto deletePeerDto) {
+        boolean deleted = false;
+        if(peerRepository.existsById(deletePeerDto.getNickname())){
+            peerRepository.deleteById(deletePeerDto.getNickname());
+            deleted = true;
+        }
+        return deleted;
+    }
+
+    @Transactional
+    public void updatePeer(PeerDto editPeerDto) {
+        Optional<PeerEntity> optionalPeerEntity = peerRepository.findById(editPeerDto.getNickname());
+        if(optionalPeerEntity.isPresent()){
+            PeerEntity peerEntity = optionalPeerEntity.get();
+            peerMapper.updatePeerFromDto(editPeerDto, peerEntity);
+            peerRepository.save(peerEntity);
+        }
+    }
+}
