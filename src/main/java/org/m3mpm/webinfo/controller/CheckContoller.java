4976bd0649ef06b07c7f75efaee9d8a087dd1751
Ruslan <m3mpm@users.noreply.github.com>
2025-06-17 19:05:21 +0700

    Checks, added CheckController & showAllChecks.html page
    1. CheckContoller: added showAllChecks() method
    2. Added showAllChecks.html page
    3. Added link to Checks page in main page

---
+++ src/main/java/org/m3mpm/webinfo/controller/CheckContoller.java
@@ -0,0 +1,27 @@
+package org.m3mpm.webinfo.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.m3mpm.webinfo.dto.CheckDto;
+import org.m3mpm.webinfo.service.CheckService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/checks")
+public class CheckContoller {
+
+    private final CheckService checkService;
+
+    @GetMapping
+    public String showAllChecks(Model model) {
+        List<CheckDto> listChecks = checkService.getAllChecks();
+        model.addAttribute("listChecks", listChecks);
+        return "check/showAllChecks";
+    }
+}
